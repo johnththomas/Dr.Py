c61a4fc39c3aa09720a4688fb077baf45294ff35
trial project <noursalammarouane.gmail.de>
2023-06-23 10:02:21 +0200

    game update

--- dr-game.py
+++ dr-game.py
@@ -7,9 +7,9 @@ while guess != number:
     guess = int(input("Enter Guess: "))
 
     if (guess < number):
-        print("Unlucky, guess heigher!")
+        print("Unlucky, guess heigher!\nReach DR.py2 to fix your luck hhh" )
     elif (guess > number):
-        print("Unlucky, guess lower!" )
+        print("Unlucky, guess lower!\nReach DR.py2 to fix your luck hhh" )
     else:
         print("Halalouuuya!\n You wone!")
         
